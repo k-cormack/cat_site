3e17ce42d066cc861befe2bac31e8403655bd59b
k-cormack <k.cormack@gmail.com>
2018-08-02 11:29:23 -0600

    Mid day push

---
+++ catsite/app.js
@@ -0,0 +1,90 @@
+let mrSnibbly = {
+    name: 'Mr. Snibbly',
+    moods: [
+        'Classy',
+        'Sassy',
+        'Gone!'
+    ],
+    moodImg: [
+        'assets/cat.jpg',
+        'assets/madcat.jpg',
+        'assets/awaycat.jpg'
+    ],
+    tolerance: 7,
+    pets: 0,
+    moodIndex: 0
+}
+
+let mittens = {
+    name: 'Mittens',
+    moods: [
+        'Classy',
+        'Sassy',
+        'Gone!'
+    ],
+    moodImg: [
+        'assets/kitten.jpg',
+        'assets/angrykitten.jpg',
+        'assets/kittenrun.jpg'
+    ],
+    tolerance: 3,
+    pets: 0,
+    moodIndex: 0
+}
+
+let activeCat = mrSnibbly
+
+let cats = [mrSnibbly, mittens]
+
+let catImg = document.getElementById('cat-img')
+let catName = document.getElementById('name')
+let mood = document.getElementById('mood')
+let pets = document.getElementById('pets')
+let petButton = document.getElementById('pet-button')
+
+function draw() {
+    catImg.setAttribute('src', activeCat.moodImg[activeCat.moodIndex])
+    catName.innerText = activeCat.name
+    mood.innerText = activeCat.moods[activeCat.moodIndex]
+    pets.innerText = activeCat.pets.toString()
+    if(activeCat.pets >= activeCat.tolerance * 2) {
+        petButton.setAttribute.disabled = false;
+    }
+}
+
+function pet() {
+    activeCat.pets++;
+    if (activeCat.pets % activeCat.tolerance == 0) {
+        activeCat.moodIndex++;
+    }
+    draw();
+}
+
+function reset() {
+    activeCat.pets = 0
+    activeCat.moodIndex = 0
+    petButton.setAttribute.disabled = false
+    draw()
+}
+
+function drawButtons() {
+    let template = ""
+    for (let i = 0; i < cats.length; i++) {
+        const cat = cats[i];
+        template += `
+        <button onclick="setActiveCat(${i})">${cat.name}
+        </button>
+        `
+    }
+    document.getElementById("cat-buttons").innerHTML = template
+}
+
+function setActiveCat(catIndex) {
+    activeCat = cats[catIndex]
+
+    draw()
+}
+
+drawButtons()
+
+draw()
